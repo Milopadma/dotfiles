3d419f46835fd741b01efd3d38793484f0ec8d5f
mylo <milopadma@yahoo.com>
2022-12-16 15:02:05 +0800

    new remaps

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -4,6 +4,21 @@ M.lspzero = {
   n = {
     -- for formattingg
     ["<leader>fm"] = { "<cmd> LspZeroFormat <CR>", "Format with lspzero" },
+
+    -- lsp stuff
+    ["F2"] = {
+      function()
+        vim.lsp.buf.rename()
+      end,
+      "rename references",
+    },
+
+    ["<leader>f"] = {
+      function()
+        vim.diagnostic.open_float()
+      end,
+      "floating diagnostic",
+    },
   },
 }
 
